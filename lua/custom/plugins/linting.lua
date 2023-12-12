c4c903ce72ec136c7ba661e379a951b3a3e45d45
chrsolr <chr.solr@gmail.com>
2023-12-12 00:06:33 -0600

    updated

--- lua/custom/plugins/linting.lua
+++ lua/custom/plugins/linting.lua
@@ -1,34 +1,33 @@
 return {
-  "mfussenegger/nvim-lint",
+  'mfussenegger/nvim-lint',
   enabled = false,
   event = {
-    "BufReadPre",
-    "BufNewFile"
+    'BufReadPre',
+    'BufNewFile',
   },
   config = function()
-    local lint = require("lint")
+    local lint = require 'lint'
 
     lint.linters_by_ft = {
-      javascript = { { "eslint_d" } },
-      typescript = { { "eslint_d" } },
-      javascriptreact = { { "eslint_d" } },
-      typescriptreact = { { "eslint_d" } },
-      c_sharp = { { "dotnet", "format" } },
+      javascript = { { 'eslint_d' } },
+      typescript = { { 'eslint_d' } },
+      javascriptreact = { { 'eslint_d' } },
+      typescriptreact = { { 'eslint_d' } },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", {
-      clear = true
+    local lint_augroup = vim.api.nvim_create_augroup('lint', {
+      clear = true,
     })
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
-      end
+      end,
     })
 
-    vim.keymap.set({ 'n', }, '<leader>fl', function()
+    vim.keymap.set({ 'n' }, '<leader>fl', function()
       lint.try_lint()
-    end, { noremap = true, desc = "Trigger linting for current file" })
-  end
+    end, { noremap = true, desc = 'Trigger linting for current file' })
+  end,
 }
