2159181de8bc44bb596775705df8e31275b138d6
chrsolr <chr.solr@gmail.com>
2024-01-27 23:31:19 -0600

    updated indent blankline

--- lua/custom/plugins/indent-blankline.lua
+++ lua/custom/plugins/indent-blankline.lua
@@ -1,12 +1,9 @@
 return {
-	-- Add indentation guides even on blank lines
-	'lukas-reineke/indent-blankline.nvim',
-	-- Enable `lukas-reineke/indent-blankline.nvim`
-	-- See `:help indent_blankline.txt`
-	main = "ibl",
-	-- version = '2.20.8',
-	opts = {
-		-- char = '┊',
-		-- show_trailing_blankline_indent = false,
-	},
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts = {
+    indent = {
+      char = '╎',
+    },
+  },
 }
