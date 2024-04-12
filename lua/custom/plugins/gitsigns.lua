764cf08312174669526d1c3a8cdf777766e0a01c
iwchrsolr <christian@innowatts.com>
2024-04-12 08:05:11 -0500

    updated

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,24 +1,24 @@
-local options = {
-  signs = {
-    add = { text = '│' },
-    change = { text = '│' },
-    delete = { text = '󰍵' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked = { text = '│' },
-  },
-}
-
 -- local options = {
---   -- See `:help gitsigns.txt`
 --   signs = {
---     add = { text = '+' },
---     change = { text = '~' },
---     delete = { text = '_' },
+--     add = { text = '│' },
+--     change = { text = '│' },
+--     delete = { text = '󰍵' },
 --     topdelete = { text = '‾' },
 --     changedelete = { text = '~' },
+--     untracked = { text = '│' },
 --   },
 -- }
+
+local options = {
+  -- See `:help gitsigns.txt`
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
 
 return {
   -- Adds git releated signs to the gutter, as well as utilities for managing changes
