931f58597b94109f4fbdf30d6c6ed2fc32ca62f1
iwchrsolr <christian@innowatts.com>
2024-04-14 14:51:52 -0500

    updated lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,15 +1,15 @@
 return {
-	"williamboman/mason.nvim",
-	opts = {
-		ensure_installed = {
-			"lua-language-server",
-			"html-lsp",
-			"prettierd",
-			"stylua",
-			"eslint_d",
-			"typescript-language-server",
-			"tailwindcss-language-server",
-			"css-lsp",
-		},
-	},
+	-- "williamboman/mason.nvim",
+	-- opts = {
+	-- 	ensure_installed = {
+	-- 		"lua-language-server",
+	-- 		"html-lsp",
+	-- 		"prettierd",
+	-- 		"stylua",
+	-- 		"eslint_d",
+	-- 		"typescript-language-server",
+	-- 		"tailwindcss-language-server",
+	-- 		"css-lsp",
+	-- 	},
+	-- },
 }
