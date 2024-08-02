92f17c791923519da5f1d6648c306de86b3ef66a
chrsolr <chr.solr@gmail.com>
2024-08-02 09:43:38 -0500

    Added lazygit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,15 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- Escape insert mode with jk
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- CMD enter command mode
+map("n", ";", ":", { desc = "Enter command mode" })
+
+-- Save file
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>", { desc = "Save buffer" })
+
+-- Lazygit
+map("n", "<leader>gg", "<cmd> LazyGit <cr>", { desc = "Open Lazy Git" })
