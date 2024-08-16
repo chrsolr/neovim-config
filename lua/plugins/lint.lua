c2976125785ea652ede2c78aa56d7ac23169efd6
iwchrsolr <christian@innowatts.com>
2024-08-16 16:59:29 -0500

    updated

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -2,6 +2,13 @@ return {
   "mfussenegger/nvim-lint",
   enabled = true,
   event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    linters = {
+      eslint_d = {
+        args = { "--no-warn-ignored", "--stdin", "--stdin-filename", "%filepath", "--format", "json" },
+      },
+    },
+  },
   config = function()
     require("configs.lint")
   end,
