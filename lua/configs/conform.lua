7d3a1a6bde634ec84ddb9f5f5961c35c736cd11a
iwchrsolr <christian@innowatts.com>
2024-08-16 16:59:29 -0500

    updated

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -7,10 +7,10 @@ local options = {
   },
 
   formatters_by_ft = {
-    javascript = { "prettierd", stop_after_first = true },
-    typescript = { "prettierd", stop_after_first = true },
-    javascriptreact = { "prettierd", stop_after_first = true },
-    typescriptreact = { "prettierd", stop_after_first = true },
+    javascript = { "biome", "prettierd", stop_after_first = true },
+    typescript = { "biome", "prettierd", stop_after_first = true },
+    javascriptreact = { "biome", "prettierd", stop_after_first = true },
+    typescriptreact = { "biome", "prettierd", stop_after_first = true },
     json = { "prettierd" },
     html = { "prettierd" },
     yaml = { "prettierd" },
