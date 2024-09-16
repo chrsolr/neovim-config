6163575cf53e4024019c83e50f0f0b28598b42c9
chrsolr <chr.solr@gmail.com>
2024-09-16 18:54:40 -0500

    reset

--- lua/plugins/mason-conform.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "zapling/mason-conform.nvim",
-  enabled = true,
-  event = "VeryLazy",
-  dependencies = { "conform.nvim" },
-  config = function()
-    require("configs.mason-conform")
-  end,
-}
