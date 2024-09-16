27dccf38293ce4b7d5b6fa19a8a4aa90f4419c65
chrsolr <chr.solr@gmail.com>
2024-09-16 18:54:40 -0500

    reset

--- lua/plugins/lspconfig.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  enabled = true,
-  config = function()
-    require("nvchad.configs.lspconfig").defaults()
-    require("configs.lspconfig")
-  end,
-}
