3e383bb852bbb8fc53ee46cc43675df2272fbcf1
iwchrsolr <christian@innowatts.com>
2024-09-17 11:18:36 -0500

    updated

--- lua/configs/neo-tree.lua
+++ lua/configs/neo-tree.lua
@@ -1,5 +1,22 @@
 require('neo-tree').setup {
   close_if_last_window = true,
-  enable_git_status = false,
+  enable_git_status = true,
   enable_diagnostics = false,
+  default_component_configs = {
+    git_status = {
+      symbols = {
+        -- Change type
+        added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted = '', -- this can only be used in the git_status source
+        renamed = '', -- this can only be used in the git_status source
+        -- Status type
+        untracked = '',
+        ignored = '',
+        unstaged = '',
+        staged = '',
+        conflict = '',
+      },
+    },
+  },
 }
