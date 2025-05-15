fda82f680ec752f65eeae4a095cea4aff7fc8305
chrsolr <chr.solr@gmail.com>
2025-05-15 18:47:13 -0500

    updated

--- init.lua
+++ init.lua
@@ -33,6 +33,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
 })
 
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+  pattern = { "*.*" },
+  desc = "save view (folds), when closing file",
+  command = "mkview",
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  pattern = { "*.*" },
+  desc = "load view (folds), when opening file",
+  command = "silent! loadview",
+})
+
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
