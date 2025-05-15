fb74db79bb5fc62c1cd23d457bc72869092a870f
chrsolr <chr.solr@gmail.com>
2025-05-15 09:00:01 -0500

    Updated

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,6 +1,6 @@
 return {
   'yetone/avante.nvim',
-  enabled = true,
+  enabled = false,
   event = 'VeryLazy',
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
@@ -8,10 +8,10 @@ return {
     -- add any opts here
     provider = 'copilot', -- Recommend using Claude
     auto_suggestions_provider = 'copilot',
-    claude = {
-      endpoint = 'https://api.anthropic.com',
-      model = 'claude-3-5-sonnet-20241022',
-    },
+    -- claude = {
+    --   endpoint = 'https://api.anthropic.com',
+    --   model = 'claude-3-5-sonnet-20241022',
+    -- },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make',
