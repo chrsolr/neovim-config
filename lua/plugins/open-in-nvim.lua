9b865675ad234eb180d6fffcacc78cf41d65144f
chrsolr <chr.solr@gmail.com>
2025-08-05 02:13:16 -0500

    updated

---
+++ lua/plugins/open-in-nvim.lua
@@ -0,0 +1,9 @@
+return {
+  "chrsolr/open-in.nvim",
+  -- dir = "~/dev/open-in.nvim",
+  lazy = false,
+  enabled = true,
+  config = function()
+    require("open-in").setup()
+  end,
+}
--- lua/plugins/open.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- "chrsolr/open.nvim",
-  dir = "~/dev/open.nvim",
-  lazy = false,
-  enabled = true,
-  config = function()
-    require("open").setup()
-  end,
-}
